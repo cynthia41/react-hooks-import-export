af67322363bcdef8ce8054d3d7816ae9cf4d72bb
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-04-19 14:38:13 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks(); // => "42 parks!
+  console.log(trees);
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div id="root"> 
+  <h1>Colorado State Parks!</h1>
+  <MesaVerde />
+  </div>
+  );
 }
+export default ColoradoStateParks
